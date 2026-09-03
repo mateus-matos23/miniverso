d6c02ecf265e1df783beabb86f467f4f10720388
Mateus Oliveira <mateus.m.oliveira2303@gmail.com>
2026-09-03 17:36:59 -0300

    feat: define Database types and integrate with Supabase client

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,11 +1,12 @@
 import { createServerClient } from "@supabase/ssr";
 import type { cookies } from "next/headers";
 import { env } from "@/config/env";
+import type { Database } from "./database";
 
 export const createClient = (
   cookieStore: Awaited<ReturnType<typeof cookies>>,
 ) => {
-  return createServerClient(
+  return createServerClient<Database>(
     env.NEXT_PUBLIC_SUPABASE_URL,
     env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
     {
